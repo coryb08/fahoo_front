25208cbab634cbe64147cfe8472d95799b1b6295
github username <github email address>
2018-02-28 11:36:02 -0500

    updated atag 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => (
   <div className="footer">
     <div className="footerContent">
-      {"https://fahoo.herokuapp.com"}
+      Fahoo
       <br />
       <br />
       Written, developed, and designed by{" "}
