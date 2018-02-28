b1acdcc46be9eb00126a97f2a5c60b2aba77de13
github username <github email address>
2018-02-28 18:52:14 -0500

    newStaticHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,45 @@
-import React from "react"
+import React, { Component } from "react"
 import SearchForm from "../containers/SearchForm"
 import * as actions from "../actions"
 import { connect } from "react-redux"
 import UserNav from "../containers/UserNav"
 
-const Header = props => {
-  function handleOnSubmit() {
-    props.fetchArticles("")
+export class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      theClass: "headerStatic"
+    }
   }
-  return (
-    <div className="header">
-      <div className="mainLogo">
-        <div
-          onClick={handleOnSubmit}
-          className="imageLogo"
+  handleOnSubmit = () => {
+    this.props.fetchArticles("")
+  }
 
-        />
+  handleScroll = () => {
+    if (window.pageYOffset > 0) {
+      this.setState({ theClass: "headerStaticShadow" })
+    } else {
+      this.setState({ theClass: "headerStaticOrig" })
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  render() {
+    return (
+      <div className={this.state.theClass}>
+        <div className="header">
+          <div className="mainLogo">
+            <div onClick={this.handleOnSubmit} className="imageLogo" />
+          </div>
+          <SearchForm />
+          <UserNav />
+        </div>
       </div>
-      <SearchForm />
-      <UserNav />
-    </div>
-  )
+    )
+  }
 }
 
 export default connect(null, actions)(Header)
